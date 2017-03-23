f71ea84086ca138dd9c79cf733ed8640564c41bd
dimonwtf <larkinhq@gmail.com>
2017-03-23 02:04:07 -0400

    Initial commit

---
+++ march26/pipelines.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import json
+import datetime
+
+# convert 2.3[K|M] to 2300,2300000
+def social_number_to_real_number(n):
+    ends = n[-1]
+    mul = 1
+    if ends == "K":
+        mul = 1000
+    if ends == "M":
+        mul = 1000000
+    if mul == 1:
+        return int(n)
+    else:
+        return int(float(n[:-1])*mul)
+
+
+def datetime_handler(x):
+    if isinstance(x, datetime.datetime):
+        return x.isoformat()
+    raise TypeError("Unknown type")
+
+
+class March26Pipeline(object):
+
+    def open_spider(self, spider):
+        self.cities_data = {}
+
+
+    def process_item(self, item, spider):
+        city_name = item['city_name']
+        if item['type'] == 'wall':
+            links = []
+            if item['link1']:
+                links.append(item['link1'])
+            if item['link2']:
+                links.append(item['link2'])
+
+            self.cities_data[city_name] = {
+                'city_name': city_name,
+                'links': links,
+                'counters': {},
+            }
+
+        if item['type'] == 'stats':
+            city = self.cities_data[city_name]
+            city['counters'][item['url']] = item['counters']
+
+        return item
+
+    def close_spider(self, spider):
+        print("Data=%s" % self.cities_data)
+
+        total_stats = {
+            'total_cities': 0,
+            'total_cities_without_stats': 0,
+            'attending': 0,
+            'maybe': 0,
+            'invited': 0,
+        }
+
+        cities_without_stats = []
+        for (city, data) in self.cities_data.items():
+            total_stats['total_cities'] += 1
+
+            for sn, c in data['counters'].items():
+                num_a = social_number_to_real_number(c['attending'])
+                num_i = social_number_to_real_number(c['invited'])
+                num_m = social_number_to_real_number(c['maybe'])
+                total_stats['attending'] += num_a
+                total_stats['maybe'] += num_m
+                total_stats['invited'] += num_i
+
+                c['attending'] = num_a
+                c['invited'] = num_i
+                c['maybe'] = num_m
+
+                if num_a == 0 and num_i == 0 and num_m == 0:
+                    total_stats['total_cities_without_stats'] += 1
+                    cities_without_stats.append({'city_name': city, 'url': sn})
+
+        stats = {
+            'stats_total': total_stats,
+            'stats_by_city': self.cities_data,
+            'cities_without_stats': cities_without_stats,
+            'updated_time': datetime.datetime.utcnow(),
+        }
+
+        with open("stats.json", "w") as outfile:
+            json.dump(stats, outfile, indent=4, default=datetime_handler)
+
---
+++ march26/spiders/spider.py
@@ -0,0 +1,99 @@
+import re
+import scrapy
+
+def group(lst, n):
+  for i in range(0, len(lst), n):
+    val = lst[i:i+n]
+    if len(val) == n:
+      yield tuple(val)
+
+
+class March26Spider(scrapy.Spider):
+    name = "march26"
+    start_urls = ["https://vk.com/wall-55284725_272730"]
+
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x26.03.2017) AppleWebKit/26.03.2017 (KHTML, like Gecko) Chrome/26.03.2017 Safari/26.03.2017'
+    }
+
+    def create_request(self, city_name, url):
+        if not url:
+            return
+
+        if url.startswith('vk.com'):
+            full_url = "https://%s" % url
+            return scrapy.Request(full_url, callback=self.parse_vk, meta={'city_name': city_name, 'url': url})
+        elif "facebook.com" in url:
+            if not url.startswith("https://") and not url.startswith("http://"):
+                full_url = "https://%s" % url
+            return scrapy.Request(full_url, callback=self.parse_fb, meta={'city_name': city_name, 'url': url})
+
+    def parse(self, response):
+        cities = []
+        raw_cities = response.css('div.wall_post_text').re(r'((\d+)\.\s{1}([^\<]+)\s{1}\<a href=[\'"]?([^\'" >]+)[^>]+\>([^\<]+)\<\/a\>( и (\<a href=[\'"]?[^\'" >]+[^>]+\>([^\<]+)\<\/a\>))?)')
+        raw_cities = group(raw_cities, 8)
+        print(raw_cities)
+        for (_, _, city_name, _, link1, _, _, link2) in raw_cities:
+            yield {
+                'type': 'wall',
+                'city_name': city_name,
+                'link1': link1,
+                'link2': link2,
+            }
+
+            req = self.create_request(city_name, link1)
+            if req:
+                yield req
+
+            req = self.create_request(city_name, link2)
+            if req:
+                yield req
+
+
+    def parse_vk(self, response):
+        stats = {
+            'type': 'stats',
+            'city_name': response.meta['city_name'],
+            'url': response.meta['url'],
+            'counters': {
+                'attending': '0',
+                'maybe': '0',
+                'invited': '0',
+            }
+        }
+
+        raw_counters = response.css('div.group_counters_wrap a.page_counter')
+        for cnt in raw_counters:
+            label = cnt.css('div.label::text').extract()[0]
+            count = cnt.css('div.count::text').extract()[0]
+            if label == 'attending':
+                stats['counters']['attending'] = count
+            elif label == 'may be':
+                stats['counters']['maybe'] = count
+            elif label == 'invited':
+                stats['counters']['invited'] = count
+
+        yield stats
+
+
+    def parse_fb(self, response):
+        stats = {
+            'type': 'stats',
+            'city_name': response.meta['city_name'],
+            'url': response.meta['url'],
+            'counters': {
+                'attending': '0',
+                'maybe': '0',
+                'invited': '0',
+            }
+        }
+
+        # 18 Going&nbsp;·&nbsp;24 Interested
+        body = response.body.decode("utf8")
+        raw_counters = re.search(r"""((\d+)((\.){1}\d+[A-Z]?)?){1} Going&nbsp;\·&nbsp;((\d+)((\.){1}\d+[A-Z]?)?){1} Interested""", body, re.MULTILINE|re.IGNORECASE)
+        if raw_counters:
+            stats['counters']['going'] = raw_counters.group(1)
+            stats['counters']['maybe'] = raw_counters.group(5)
+
+        yield stats
+
